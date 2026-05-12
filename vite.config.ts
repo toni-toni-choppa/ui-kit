a8335f52befb88c1e3b9a2f48efd3ba6b4962746
Tony Tony Chopper <241455787+toni-toni-choppa@users.noreply.github.com>
2026-05-12 19:00:38 -0400

    feat: update package.json for scoped package name and add exports, add styles and entry point for library

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'UIKit',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `ui-kit.${format}.js`,
+    },
+
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+    },
+  },
 })
