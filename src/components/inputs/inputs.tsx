e0609f5f83ef05298cd35d60d96628b9e7dbf1c7
Tony Tony Chopper <241455787+toni-toni-choppa@users.noreply.github.com>
2026-05-14 19:05:50 -0400

    feat: add textarea component with styles and functionality

--- src/components/inputs/inputs.tsx
+++ src/components/inputs/inputs.tsx
@@ -1,9 +1,11 @@
 import Text from "./text";
 import Select from "./select";
+import TextArea from "./textarea";
 
 const Input = {
     Text,
-    Select
+    Select,
+    TextArea
 }
 
 export default Input;
---
+++ src/components/inputs/textarea.tsx
@@ -0,0 +1,22 @@
+import input from "./input.module.css";
+
+type TextAreaProps = {
+    placeholder?: string;
+    value?: string;
+    id: string;
+    onChange?: (value: string) => void;
+    disabled?: boolean;
+};
+
+export default function TextArea({ placeholder, value, id, onChange, disabled }: TextAreaProps) {
+    return (
+        <textarea
+            className={input.textInput}
+            id={id}
+            disabled={disabled}
+            placeholder={placeholder}
+            value={value}
+            onChange={(e) => onChange?.(e.target.value)}
+        />
+    );
+}
