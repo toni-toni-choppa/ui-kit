265a906bfd8bd2f75094d4591183da40e178473f
Tony Tony Chopper <241455787+toni-toni-choppa@users.noreply.github.com>
2026-06-03 21:03:32 -0400

    feat: add icon support to Button component and update props structure

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -1,20 +1,24 @@
 import button from './button.module.css'
 
 type ButtonProps = {
-  label: string;
+  icon?: React.ReactNode;
+  label?: string;
   onClick: () => void;
   variant?: 'primary' | 'secondary' | 'transparent';
   size?: 'small' | 'medium' | 'large';
   disabled?: boolean;
 };
 
-export default function Button({ label, onClick, variant = 'primary', size = 'medium', disabled = false }: ButtonProps) {
+export default function Button({ icon, label, onClick, variant = 'primary', size = 'medium', disabled = false }: ButtonProps) {
   return (
     <button
       className={`${button.button} ${button[variant]} ${button[size]}`}
       onClick={onClick}
       disabled={disabled}
     >
+      {
+        icon ? icon : null
+      }
       {label}
     </button>
   );
